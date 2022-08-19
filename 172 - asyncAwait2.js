f7075b03301dad4e40eb3821783a1e0ca3fd2d86
igorhit <igor_hit@yahoo.com.br>
2022-08-19 14:47:59 -0300

    Add files via upload

---
+++ 172 - asyncAwait2.js
@@ -0,0 +1,36 @@
+function gerarNumerosEntre(min, max, numeroProibido) {
+    if(min > max) [max, min] = [min, max]
+    return new Promise((resolve, reject) => {
+        const fator = max - min + 1
+        const aleatorio = parseInt(Math.random() * fator) + min
+        if(numeroProibido.includes(aleatorio)) {
+            reject('Número repetido!')
+        } else {
+            resolve(aleatorio)
+        }
+    })
+}
+
+async function gerarMegaSena(qtdNumeros, tentativas = 1) {
+    try {
+        const numeros = []
+        for(let _ of Array(qtdNumeros).fill()) {
+            numeros.push(await gerarNumerosEntre(1, 60, numeros))
+        }
+        return numeros
+    } catch(e) {
+        if(tentativas > 10) {
+            throw "Não deu certo!"
+        } else {
+            gerarMegaSena(qtdNumeros, tentativas + 1)
+        }  
+    }
+}
+
+gerarMegaSena(15)
+    .then(console.log)
+    .catch(console.log)
+
+// gerarNumerosEntre(1, 5, [1, 2, 4])
+//     .then(console.log)
+//     .catch(console.log)
